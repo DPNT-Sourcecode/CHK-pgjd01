741afa95b3aa94d69d00be05d797f0733a8d409d
sbx_user1051 <sbx_user1051@169.254.3.51>
2025-10-25 12:09:05 +0000

    Sat Oct 25 12:09:05 UTC 2025

--- src/test/java/io/accelerate/solutions/CHK/basket/ItemTest.java
+++ src/test/java/io/accelerate/solutions/CHK/basket/ItemTest.java
@@ -1,8 +1,25 @@
 package io.accelerate.solutions.CHK.basket;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ItemTest {
 
+    @Test
+    public void shouldCreateItem() {
+        Item validItem = Item.fromInput("2C");
 
+        assertEquals(ItemType.C, validItem.getType());
+        assertEquals(2, validItem.getCount());
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenInvalid() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () ->Item.fromInput("invalid2")
+        );
+    }
 }
