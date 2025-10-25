567476b6b5e35a132e839b8d37d5ef2c5a9e5d3c
sbx_user1051 <sbx_user1051@169.254.3.51>
2025-10-25 11:45:07 +0000

    Sat Oct 25 11:45:07 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/checkout/CheckoutCalculatorService.java
+++ src/main/java/io/accelerate/solutions/CHK/checkout/CheckoutCalculatorService.java
@@ -1,2 +1,10 @@
-package io.accelerate.solutions.CHK.checkout;public class CheckoutCalculatorService {
+package io.accelerate.solutions.CHK.checkout;
+
+import io.accelerate.solutions.CHK.basket.Basket;
+
+public class CheckoutCalculatorService {
+
+    public int calculate(Basket basket) {
+        return 0;
+    }
 }
