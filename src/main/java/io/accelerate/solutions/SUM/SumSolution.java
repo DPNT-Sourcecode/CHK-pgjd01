0c08f8037f56b2eb7286fb529d299b8fbe9e67aa
sbx_user1051 <sbx_user1051@169.254.3.51>
2025-10-25 10:37:46 +0000

    Sat Oct 25 10:37:46 UTC 2025

--- src/main/java/io/accelerate/solutions/SUM/SumSolution.java
+++ src/main/java/io/accelerate/solutions/SUM/SumSolution.java
@@ -1,13 +1,15 @@
 package io.accelerate.solutions.SUM;
 
-import io.accelerate.runner.SolutionNotImplementedException;
-
 @SuppressWarnings("unused")
 public class SumSolution {
 
     public int compute(int x, int y) {
+        if (x < 0 || x > 100 || y < 0 || y > 100) {
+            throw new IllegalArgumentException("Arguments must be between 0 and 100");
+        }
         return x + y;
     }
 
 }
+
 
