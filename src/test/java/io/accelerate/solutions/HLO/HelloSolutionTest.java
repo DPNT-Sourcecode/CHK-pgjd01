e51713370acd4bc922a3b69d1e1fd6a8ab17abe5
sbx_user1051 <sbx_user1051@169.254.3.51>
2025-10-25 10:54:53 +0000

    Sat Oct 25 10:54:53 UTC 2025

--- src/test/java/io/accelerate/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/io/accelerate/solutions/HLO/HelloSolutionTest.java
@@ -1,4 +1,23 @@
-import static org.junit.jupiter.api.Assertions.*;
+package io.accelerate.solutions.HLO;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 class HelloSolutionTest {
-  
+
+    private HelloSolution helloSolution;
+
+    @BeforeEach
+    public void setUp() {
+        helloSolution = new HelloSolution();
+    }
+
+    @Test
+    public void shouldComputeHello() {
+        assertThat(helloSolution.hello("mellon"), equalTo("Hello, mellon!"));
+    }
+
 }
