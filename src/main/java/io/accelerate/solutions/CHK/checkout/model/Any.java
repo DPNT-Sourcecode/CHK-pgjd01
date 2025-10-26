905f2420d27dc438f82728e0b5222284889f7a85
sbx_user1051 <sbx_user1051@169.254.40.77>
2025-10-26 19:11:19 +0000

    Sun Oct 26 19:11:19 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/checkout/model/Any.java
+++ src/main/java/io/accelerate/solutions/CHK/checkout/model/Any.java
@@ -1,2 +1,22 @@
-package io.accelerate.solutions.CHK.checkout.model;public class Any {
+package io.accelerate.solutions.CHK.checkout.model;
+
+import io.accelerate.solutions.CHK.basket.model.ItemType;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Any {
+
+    private final Set<ItemType> items;
+
+    public static Any of(ItemType... types) {
+        return new Any(Arrays.stream(types).collect(Collectors.toSet()));
+    }
 }
+
