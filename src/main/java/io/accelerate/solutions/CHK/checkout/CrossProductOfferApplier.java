60ecf948e67e7dace8ad8b7dd9c8ff48833f4a39
sbx_user1051 <sbx_user1051@169.254.49.113>
2025-10-26 21:50:48 +0000

    Sun Oct 26 21:50:48 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/checkout/CrossProductOfferApplier.java
+++ src/main/java/io/accelerate/solutions/CHK/checkout/CrossProductOfferApplier.java
@@ -4,12 +4,20 @@ import io.accelerate.solutions.CHK.basket.model.ItemCount;
 import io.accelerate.solutions.CHK.checkout.model.SpecialOffer;
 import io.accelerate.solutions.CHK.checkout.model.SpecialOfferResult;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class CrossProductOfferApplier {
 
     public SpecialOfferResult apply(List<ItemCount> itemsInBasket, SpecialOffer crossProductOffer) {
+        List<ItemCount> applicableItems = new ArrayList<>(itemsInBasket.stream()
+                .filter(item -> crossProductOffer.getTargetTypes().contains(item.getType())).toList());
+
+        applicableItems.sort(Comparator.comparing(ItemCount::computeBasePrice).reversed());
+
         return null;
     }
 }
+
 
