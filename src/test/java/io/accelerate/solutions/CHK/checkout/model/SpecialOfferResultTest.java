3f3ad18b1b1b822408f9d1b71c4a28fce5e482f7
sbx_user1051 <sbx_user1051@169.254.49.113>
2025-10-26 22:52:06 +0000

    Sun Oct 26 22:52:06 UTC 2025

---
+++ src/test/java/io/accelerate/solutions/CHK/checkout/model/SpecialOfferResultTest.java
@@ -0,0 +1,25 @@
+package io.accelerate.solutions.CHK.checkout.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpecialOfferResultTest {
+
+    @Test
+    void getAmountAppliedTo() {
+        SpecialOffer.of()
+    }
+
+    @Test
+    void getTotalDiscount() {
+    }
+
+    @Test
+    void getItemsOfferWasAppliedTo() {
+    }
+
+    @Test
+    void getTotalPriceOfBundle() {
+    }
+}
